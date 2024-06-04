d87a042b676284514745af0e9cdaf4b6234f7ffe
Koosha Samadi <42487233+KooshaSamadi@users.noreply.github.com>
2024-06-04 12:45:54 +0200

    Datasets have been changed

--- project/data_pipeline.py
+++ project/data_pipeline.py
@@ -3,56 +3,95 @@ import zipfile
 import os
 import pandas as pd
 import sqlite3
+import tempfile
 
-# Function to download dataset from a URL
+#download dataset from a URL
 def download_dataset_from_url(url, download_path):
     response = requests.get(url)
-    with open(download_path, 'wb') as file:
+    with open(download_path, "wb") as file:
         file.write(response.content)
 
-# Function to extract zip files
+
+#extract zip files
 def extract_zip(zip_path, extract_to):
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(extract_to)
 
-# Function to clean data
-def clean_data(file_path):
-    df = pd.read_csv(file_path)
-    print("Column names:", df.columns)
+
+def process_data(file_path):
+    try:
+        df = pd.read_csv(file_path, encoding="utf-8") 
+    except UnicodeDecodeError:
+        df = pd.read_csv(
+            file_path, encoding="ISO-8859-1"
+        )
+    start_year = "Y1961"
+    end_year = "Y1999"
+    print(
+        f"number of rows before processing: {df.shape[0]} and number of columns before processing: {df.shape[1]}"
+    )
+    # drop column between start_year and end_year
+    df.drop(df.loc[:, start_year:end_year].columns, axis=1, inplace=True)
+    df.rename(columns={'Y2030':'Y2022','Y2050':'Y2023'}, inplace=True)
+    #drop Area Code and Area Code (M49) and Item Code and Element Code and Source Code and Source from emmision data
+    if 'Item Code' in df.columns:
+        df.drop(['Area Code','Area Code (M49)','Item Code','Element Code','Source Code','Source'], axis=1, inplace=True)
+    #drop Area Code and Area Code (M49) and Months Code and Element Code from temperature data
+    if 'Months Code' in df.columns:
+        df.drop(['Area Code','Area Code (M49)','Months Code','Element Code'], axis=1, inplace=True)
+        #drop rows that Element == Standard Deviation
+        df = df[df.Element != 'Standard Deviation']
+   
+    return df
+
+def clean_data(df):
     df.dropna(inplace=True)
     return df
 
-# Function to save DataFrame to SQLite
-def save_to_sqlite(db_path, table_name, dataframe):
-    # Connect to SQLite database
+
+def save_to_sqlite(df, db_path):
     conn = sqlite3.connect(db_path)
-    # Write records stored in a DataFrame to a SQL database
-    dataframe.to_sql(table_name, conn, if_exists='replace', index=False)
-    # Close the connection
+    df.to_sql("data", conn, if_exists="replace", index=False)
     conn.close()
 
-# Main function
+
 def main():
-    #forest area dataset
-    #historical emissions dataset
-    download_url = ['https://storage.googleapis.com/kaggle-data-sets/4582549/7821337/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240522%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240522T193403Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=912629c7ef8a913efc774a42ec1c5ca576bcc9cf267cd66ff9c5d9832fcd07513b53a12b4cb55acbe4bfde087a73b19e5d6a3560dafa09485d4bbc4739b67e303cced5005fddac0e6cca45e07a3eda63b2e3041053658deae47bd0a8f015e4bbff2b0855163e913cfa0e8a3b5fba3e77dc5c31e7520a9decdc905fd955aedfc6279c412d24ae568b542f65372924130a8af04414604d68eb5514513c1ae7b02d2fb7214e9e6cbd610924a460abe5972db5726aa0d7997338e65bc87fef0302cd8b852af632a060885aad762a269516db6618282ac87cc481f53bea913498bb190b806491ae0d70ecc9dfa95539414731564fbf668311b54322b416aeea35dcea',
-                    'https://storage.googleapis.com/kaggle-data-sets/2020597/3348143/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20240522%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240522T193351Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=69be2800f739a0e9b1f95b25db0e22d4b4a3a79309f4b427c0c5fb6ef5b0a5bee8ad294256d361c8f51a887159ba827c1398bba2bf898d600ba9882034dd77458bff484b53e11e8a80afd7b8028cd8de75c61107cefa3a1ab24e30ef7e2a283f1d7648f95190309d98e623e4f3c2cd92a503f88ca818a1cd478894ef770e58f3591a5879e0c0588d9b2be4bda47a2b6c047a554b710046c590f4f53ed39cd938824c3bb39474972691dc031bbfb04301d410ec0567a99e8a6afc7d457e8148693a65019c579a618789f635c590d0be35f21d3d7999cb1230e19a7eb7717fcd4273c627ffdf94889c8454d681ef11b8e45e784c657cc750e687ff2bc4c2f6303f']
-    zip_file_path = ['Forest_Area.zip','historical_emissions.zip']
-    extract_to_path = './'
-    db_path = ['Forest_Area.db','historical_emissions.db']  
-    for url, zip_file_path, db_path in zip(download_url, zip_file_path, db_path):
-        download_dataset_from_url(url, zip_file_path)
-        print("Extracting dataset...")
-        extract_zip(zip_file_path, extract_to_path)
-        print("Processing data...")
-        base_file_name = os.path.basename(zip_file_path).split('.')[0]
-        data_files = [f for f in os.listdir(extract_to_path) if f.startswith(base_file_name) and f.endswith('.csv')]
-        for file in data_files:
-            file_path = os.path.join(extract_to_path, file)
-            df_cleaned = clean_data(file_path)
-            print(f"Data from {file} cleaned")
-            table_name = os.path.splitext(file)[0]
-            save_to_sqlite(db_path, table_name, df_cleaned)
+    download_url = [
+        "https://bulks-faostat.fao.org/production/Environment_Temperature_change_E_Europe.zip",
+        "https://bulks-faostat.fao.org/production/Emissions_Totals_E_Europe.zip",
+    ]
 
-
+    save_to_path = "../data"
+    database_names = ["temperture_change.db", "emissions_totals.db"]
+    #if there is no data folder, let's create one
+    if not os.path.exists(save_to_path):
+        os.makedirs(save_to_path)
+    #use temporary directory to store the downloaded files
+    with tempfile.TemporaryDirectory() as extract_to_path:
+        for url, database_name in zip(download_url, database_names):
+            zip_file_path = os.path.join(extract_to_path, os.path.basename(url))
+            download_dataset_from_url(url, zip_file_path)
+            print(f"Extracting dataset {zip_file_path}")
+            extract_zip(zip_file_path, extract_to_path)
+            print("Processing data...")
+            base_file_name = os.path.basename(zip_file_path).split(".")[0]
+            # print(base_file_name)
+            for f in os.listdir(extract_to_path):
+                if f.startswith(base_file_name) and f.endswith("NOFLAG.csv"):
+                    data_file = f
+            # print(data_file)
+            file_path = os.path.join(extract_to_path, data_file)
+            df_processed = process_data(file_path)
+            print(
+                f"number of rows after processing: {df_processed.shape[0]} and number of columns after processing: {df_processed.shape[1]}"
+            )
+            df_cleaned = clean_data(df_processed)
+            print(
+                f"number of rows after cleaning: {df_cleaned.shape[0]} and number of columns after cleaning: {df_cleaned.shape[1]}"
+            )
+            #save to ../data folder
+            save_to_sqlite(df_cleaned, os.path.join(save_to_path, database_name))
+            print("Data saved to SQLite database")
+            print("#############################################")
+                
 main()
